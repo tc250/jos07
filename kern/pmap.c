2709535617a1794f917d1a8ae9eaca270d71c8af
Tong Cheng <tcheng@hust.edu.cn>
2016-03-05 22:17:29 +0800

    pmap: alloc and map `envs'

--- kern/pmap.c
+++ kern/pmap.c
@@ -184,6 +184,7 @@ i386_vm_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+	envs = boot_alloc(NENV * sizeof(struct Env), PGSIZE);
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -217,6 +218,8 @@ i386_vm_init(void)
 	// Permissions:
 	//    - envs itself -- kernel RW, user NONE
 	//    - the image of envs mapped at UENVS  -- kernel R, user R
+	n = ROUNDUP(NENV * sizeof(struct Env), PGSIZE);
+	boot_map_segment(pgdir, UENVS, n, PADDR(envs), PTE_U);
 
 
 	//////////////////////////////////////////////////////////////////////
