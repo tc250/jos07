e812cd16fc679a6ea04d96801a7c31b4d16714ac
Tong Cheng <tcheng@hust.edu.cn>
2016-03-06 10:26:23 +0800

    env: add segment_alloc

--- kern/env.c
+++ kern/env.c
@@ -207,6 +207,14 @@ segment_alloc(struct Env *e, void *va, size_t len)
 	// Hint: It is easier to use segment_alloc if the caller can pass
 	//   'va' and 'len' values that are not page-aligned.
 	//   You should round va down, and round len up.
+	va = ROUNDDOWN(va, PGSIZE);
+	len = ROUNDUP(len, PGSIZE);
+	int d;
+	struct Page *pp;
+	for (d = 0; d < len; d += PGSIZE) {
+		assert( page_alloc(&pp) == 0);
+		assert( page_insert(e->env_pgdir, pp, va+d, PTE_U | PTE_W) == 0);
+	}
 }
 
 //
