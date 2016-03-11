d2c584cc016a925ea61b7939a6efd02ce5c4d9bc
Tong Cheng <tcheng@hust.edu.cn>
2016-03-11 20:20:50 +0800

    libmain: initialise `env'

--- lib/libmain.c
+++ lib/libmain.c
@@ -13,7 +13,9 @@ libmain(int argc, char **argv)
 {
 	// set env to point at our env structure in envs[].
 	// LAB 3: Your code here.
-	env = 0;
+	envid_t cu_env_id = sys_getenvid();
+	env = &envs[ENVX(cu_env_id)];
+	assert(env->env_id == cu_env_id);
 
 	// save the name of the program so that panic() can use it
 	if (argc > 0)
