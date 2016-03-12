22603d7cb9e9207b9356852be57562724ddacc97
Tong Cheng <tcheng@hust.edu.cn>
2016-03-12 19:06:52 +0800

    trap: handle page fault happened in kernel mode

--- kern/trap.c
+++ kern/trap.c
@@ -225,6 +225,8 @@ page_fault_handler(struct Trapframe *tf)
 	// Handle kernel-mode page faults.
 	
 	// LAB 3: Your code here.
+	if ((tf->tf_cs & 3) != 3)
+		panic("page fault in kernel mode");
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
