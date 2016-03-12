c212ffa97650dbe7211ce5323681c5b841f9df91
Tong Cheng <tcheng@hust.edu.cn>
2016-03-12 20:49:15 +0800

    syscall: sanity check arguments

--- kern/syscall.c
+++ kern/syscall.c
@@ -80,6 +80,7 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 	// LAB 3: Your code here.
 	switch (syscallno) {
 		case SYS_cputs:
+			user_mem_assert(curenv, (const char *)a1, (size_t)a2, 0);
 			sys_cputs((const char *)a1, (size_t)a2);
 			return 0; // [?]
 		case SYS_cgetc:
